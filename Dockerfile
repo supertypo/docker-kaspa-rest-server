d0070791156e0fa2d56787255f3365e75697bc88
supertypo <mthoring@protonmail.com>
2022-09-27 00:40:33 +0200

    Build only on git remote changes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.10-alpine
 
+ARG REPO_DIR
+
 EXPOSE 8000
 
 ENV KASPAD_HOST_1=n.seeder1.kaspad.net:16110
@@ -10,23 +12,23 @@ RUN apk --no-cache add \
   libc-dev \
   build-base \
   linux-headers \
+  libpq-dev \
   dumb-init
 
 RUN pip install \
   pipenv
 
-RUN mkdir -p /app \
-  && adduser -h /app -S -D -u 55747 api \
-  && chown api /app
+RUN addgroup -S -g 55747 api \
+  && adduser -h /app -S -D -g '' -G api -u 55747 api 
 
 WORKDIR /app
 USER api
 
-RUN git clone https://github.com/lAmeR1/kaspa-rest-server.git /app 2>&1
+COPY --chown=api:api "$REPO_DIR" /app
 
 RUN pipenv install --deploy 
-
-ENTRYPOINT ["/usr/bin/dumb-init", "--"]
-
-CMD pipenv run gunicorn -b 0.0.0.0:8000 -w 1 -k uvicorn.workers.UvicornWorker main:app
+#
+#ENTRYPOINT ["/usr/bin/dumb-init", "--"]
+#
+#CMD pipenv run gunicorn -b 0.0.0.0:8000 -w 1 -k uvicorn.workers.UvicornWorker main:app
 
--- build.sh
+++ build.sh
@@ -1,14 +1,34 @@
 #!/bin/sh
 PUSH=$1
 
+BUILD_DIR="$(dirname $0)"
+REPO_URL="https://github.com/lAmeR1/kaspa-rest-server.git"
+REPO_DIR="$BUILD_DIR/work/kaspa-rest-server"
+
 set -e
 
-docker build --pull -t supertypo/kaspa-rest-server:latest $(dirname $0)
-docker tag supertypo/kaspa-rest-server:latest supertypo/kaspa-rest-server:$(date +"%Y-%m-%d")
-echo Tagged supertypo/kaspa-rest-server:$(date +"%Y-%m-%d")
+if [ ! -d "$REPO_DIR" ]; then
+  git clone "$REPO_URL" "$REPO_DIR"
+  echo $(cd "$REPO_DIR" && git reset --hard HEAD~1)
+fi
 
-if [ "$PUSH" = "push" ]; then
-  docker push supertypo/kaspa-rest-server:$(date +"%Y-%m-%d")
-  docker push supertypo/kaspa-rest-server:latest
+if $(cd "$REPO_DIR" && git pull | grep -qv "up to date"); then
+  commitId=$(cd "$REPO_DIR" && git log -n1 --format="%h")
+  tag=$(date -u +"%Y-%m-%d").$commitId
+  echo
+  echo "Git repo changed, building tag '$tag'."
+  echo
+
+  docker build --pull --build-arg REPO_DIR="$REPO_DIR" -t supertypo/kaspa-rest-server:$tag "$BUILD_DIR"
+  docker tag supertypo/kaspa-rest-server:$tag supertypo/kaspa-rest-server:latest
+  echo Tagged supertypo/kaspa-rest-server:latest
+
+  if [ "$PUSH" = "push" ]; then
+    docker push supertypo/kaspa-rest-server:$tag
+    docker push supertypo/kaspa-rest-server:latest
+  fi
+else
+  commitId=$(cd "$REPO_DIR" && git log -n1 --format="%h")
+  echo "Git repo is still at '$commitId', skipping build."
 fi
 
