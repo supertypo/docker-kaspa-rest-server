6d4049378771b642010eed620137d41caa9c8e8c
Marius <suprtypo@pm.me>
2023-08-31 16:39:05 +0200

    Multi arch

--- Dockerfile
+++ Dockerfile
@@ -1,35 +1,64 @@
-FROM python:3.10-alpine
+##
+# builder image
+##
+FROM python:3.10-slim AS builder
 
 ARG REPO_DIR
 
-EXPOSE 8000
+ENV APP_USER=api \
+  APP_UID=55747 \
+  APP_DIR=/app
 
-ENV KASPAD_HOST_1=n.seeder1.kaspad.net:16110
-ENV SQL_URI=postgresql+asyncpg://postgres:password@postgresql:5432/postgres
-
-RUN apk --no-cache add \
-  git \
-  gcc \
-  libc-dev \
-  build-base \
-  linux-headers \
-  libpq-dev \
-  dumb-init
+RUN apt-get update && \
+  apt-get install -y --no-install-recommends \
+    gcc \
+    libc6-dev \
+    libpq-dev
 
 RUN pip install \
   pipenv
 
-RUN addgroup -S -g 55747 api \
-  && adduser -h /app -S -D -g '' -G api -u 55747 api 
+RUN groupadd -r -g $APP_UID $APP_USER && \
+  useradd -d $APP_DIR -r -m -s /sbin/nologin -g $APP_USER -u $APP_UID $APP_USER
 
-WORKDIR /app
-USER api
+WORKDIR $APP_DIR
+USER $APP_USER
 
-COPY --chown=api:api "$REPO_DIR" /app
+COPY --chown=$APP_USER:$APP_USER "$REPO_DIR" $APP_DIR
 
-RUN pipenv install --deploy
+RUN pipenv install --deploy -v
+RUN rm -r .cache/
+
+##
+# kaspa-db-filler image
+##
+FROM python:3.10-slim
+
+ENV APP_USER=api \
+  APP_UID=55747 \
+  APP_DIR=/app
+
+ENV KASPAD_HOST_1=kaspad:16110 \
+  SQL_URI=postgresql+asyncpg://postgres:password@postgresql:5432/postgres
+
+RUN apt-get update && \
+  apt-get install -y --no-install-recommends \
+    libpq-dev \
+    dumb-init && \
+  rm -rf /var/lib/apt/lists/*
+
+RUN pip install \
+  pipenv
+
+RUN groupadd -r -g $APP_UID $APP_USER && \
+  useradd -d $APP_DIR -r -m -s /sbin/nologin -g $APP_USER -u $APP_UID $APP_USER
+
+WORKDIR $APP_DIR
+USER $APP_USER
 
 ENTRYPOINT ["/usr/bin/dumb-init", "--"]
 
-CMD pipenv run gunicorn -b 0.0.0.0:8000 -w 1 -k uvicorn.workers.UvicornWorker main:app --timeout 120
+CMD pipenv run gunicorn -b 0.0.0.0:8000 -w 4 -k uvicorn.workers.UvicornWorker main:app --timeout 120
+
+COPY --chown=$APP_USER:$APP_USER --from=builder $APP_DIR $APP_DIR
 
--- build.sh
+++ build.sh
@@ -1,28 +1,84 @@
 #!/bin/sh
-PUSH=$1
 
 DOCKER_REPO=supertypo/kaspa-rest-server
+ARCHES="linux/amd64 linux/arm64"
 
 BUILD_DIR="$(dirname $0)"
+PUSH=$1
+TAG=${2:-main}
 REPO_URL="https://github.com/lAmeR1/kaspa-rest-server.git"
-REPO_DIR="$BUILD_DIR/work/kaspa-rest-server"
+REPO_DIR="work/kaspa-rest-server"
 
 set -e
 
-if [ ! -d "$REPO_DIR" ]; then
-  git clone "$REPO_URL" "$REPO_DIR"
-  echo $(cd "$REPO_DIR" && git reset --hard HEAD~1)
+if [ ! -d "$BUILD_DIR/$REPO_DIR" ]; then
+  git clone "$REPO_URL" "$BUILD_DIR/$REPO_DIR"
+  echo $(cd "$BUILD_DIR/$REPO_DIR" && git reset --hard HEAD~1)
 fi
 
-(cd "$REPO_DIR" && git pull)
-tag=$(cd "$REPO_DIR" && git log -n1 --format="%cs.%h")
+echo "===================================================="
+echo " Pulling $REPO_URL"
+echo "===================================================="
 
-docker build --pull --build-arg REPO_DIR="$REPO_DIR" -t $DOCKER_REPO:$tag "$BUILD_DIR"
-docker tag $DOCKER_REPO:$tag $DOCKER_REPO:latest
-echo Tagged $DOCKER_REPO:latest
+(cd "$BUILD_DIR/$REPO_DIR" && git fetch && git checkout $TAG && (git pull 2>/dev/null | true))
+
+tag=$(cd "$BUILD_DIR/$REPO_DIR" && git log -n1 --format="%cs.%h")
+
+docker=docker
+id -nG $USER | grep -qw docker || docker="sudo $docker"
+
+plain_build() {
+  echo
+  echo "===================================================="
+  echo " Running current arch build"
+  echo "===================================================="
+  $docker build --pull \
+    --build-arg REPO_DIR="$REPO_DIR" \
+    --tag $DOCKER_REPO:$tag "$BUILD_DIR"
+
+  $docker tag $DOCKER_REPO:$tag $DOCKER_REPO:multi-arch
+  echo Tagged $DOCKER_REPO:multi-arch
+
+  if [ "$PUSH" = "push" ]; then
+    $docker push $DOCKER_REPO:$tag
+    $docker push $DOCKER_REPO:multi-arch
+  fi
+  echo "===================================================="
+  echo " Completed current arch build"
+  echo "===================================================="
+}
+
+multi_arch_build() {
+  echo
+  echo "===================================================="
+  echo " Running multi arch build"
+  echo "===================================================="
+  dockerRepoArgs=
+  if [ "$PUSH" = "push" ]; then
+    dockerRepoArgs="$dockerRepoArgs --push"
+  fi
+  $docker buildx build --pull --platform=$(echo $ARCHES | sed 's/ /,/g') $dockerRepoArgs \
+    --build-arg REPO_DIR="$REPO_DIR" \
+    --tag $DOCKER_REPO:$tag \
+    --tag $DOCKER_REPO:multi-arch "$BUILD_DIR"
+  echo "===================================================="
+  echo " Completed multi arch build"
+  echo "===================================================="
+}
 
 if [ "$PUSH" = "push" ]; then
-  docker push $DOCKER_REPO:$tag
-  docker push $DOCKER_REPO:latest
+  echo
+  echo "===================================================="
+  echo " Setup multi arch build ($ARCHES)"
+  echo "===================================================="
+  if $docker buildx create --name=mybuilder --append --node=mybuilder0 --platform=$(echo $ARCHES | sed 's/ /,/g') --bootstrap --use 1>/dev/null 2>&1; then
+    echo "SUCCESS - doing multi arch build"
+    multi_arch_build
+  else
+    echo "FAILED - building on current arch"
+    plain_build
+  fi
+else
+  plain_build
 fi
 
